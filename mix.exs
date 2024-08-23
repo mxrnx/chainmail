d00e62e1cef55948931ae45963f628b55e323b5f
Marnix Massar <22885508+mxrnx@users.noreply.github.com>
2024-08-23 11:57:16 +0200

    Upgrade to elixir 1.17

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Chain.MixProject do
     [
       app: :chain,
       version: "0.1.0",
-      elixir: "~> 1.16",
+      elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: []
     ]
